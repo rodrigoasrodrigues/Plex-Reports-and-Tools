f07269f3c1476b8a9b977ce02d3264309db2a0bd
Rodrigo Rodrigues <rodrigo@rodrigorodrigues.info>
2022-05-05 23:05:56 -0300

    improving email report data

--- dependencies/admin-media-report/email_utils.py
+++ dependencies/admin-media-report/email_utils.py
@@ -5,12 +5,35 @@ import requests
 def apply_template(dic_library, template_file):
     with open(template_file, "r") as f:
         template = f.read()
+        template = apply_grouploop(dic_library, template)
         for k,v in dic_library.items():
             template = template.replace(f'{{{{{k}}}}}',str(v))
         return template
 
+
+def apply_grouploop(dic_library, template):
+    if not '{{begin.grouploop}}' in template:
+        return template
+    template_pre = template.split('{{begin.grouploop}}')[0]
+    template_pos = template.split('{{begin.grouploop}}')[1]
+    template_line = template_pos.split('{{end.grouploop}}')[0]
+    template_pos = template_pos.split('{{end.grouploop}}')[1]
+    groups = dic_library['metric_groups']
+    line_results = ""
+    for group in groups:
+        line = template_line.replace('{{metric_key}}',f'<b>{str(group)}</b>')
+        line = line.replace('{{metric_val}}','')
+        line = line.replace('{{metric_percent}}','')
+        line_results += line
+        for group_item in dic_library[group]:
+            line = template_line.replace('{{metric_key}}',str(group_item))
+            line = line.replace('{{metric_val}}',str(dic_library[group_item]))
+            line = line.replace('{{metric_percent}}',str(dic_library[f'{group_item}_percent']))
+            line_results += line
+    return template_pre + line_results + template_pos
+
 def send_admin_report(content, config):
-    print(render_mjml_template(content))
+    # print(render_mjml_template(content))
     requests.post(
         f"https://api.mailgun.net/v3/{config['mailgun']['domain']}/messages",
         auth=("api", config['mailgun']['apikey']),
--- dependencies/admin-media-report/report-status.py
+++ dependencies/admin-media-report/report-status.py
@@ -1,7 +1,8 @@
+from genericpath import isdir
 from pprint import pprint
 from pymediainfo import MediaInfo
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 from email_utils import *
 from datetime import date, timedelta
 import time
@@ -38,28 +39,36 @@ def get_media_info(path, libname):
             result['readable_file_size'] = track.other_file_size[0]
         elif track.track_type == 'Video':
             # print(track.to_data())
-            codec = track.internet_media_type.split('/')[-1]
+            codec = track.internet_media_type
+            if codec is None:
+                codec = track.commercial_name
+            elif '/' in codec:
+                codec = codec.split('/')[-1]
             result['codec'] = codec
             result['resolution'] = get_resolution(track.width)
     result['size_duration_ratio'] = result['file_size'] / result['duration']
     return result
+
+def calculate_group_metrics(df, group):
+    total = len(df)
+    group_metrics = df.groupby([group]).size()
+    group_items = group_metrics.keys()
+    metrics = {}
+    metrics[group] = group_items
+    for group_item in group_items:
+        metrics[group_item] = group_metrics[group_item]
+        metrics[f'{group_item}_percent'] = str(round(group_metrics[group_item] * 100 / total, 2)) + '%'
+    return metrics
 
 def calculate_library_metrics(df, libname):
     metrics = {}
     metrics['libname'] = libname
     total = len(df)
     metrics['total'] = total
-    codec_metrics = df.groupby(['codec']).size()
-    metrics['H264'] = codec_metrics['H264']
-    metrics['H264_PERCENT'] = codec_metrics['H264'] / total
-    metrics['H265'] = codec_metrics['H265']
-    metrics['H265_PERCENT'] = codec_metrics['H265'] / total
-    extension_metrics = df.groupby(['extension']).size()
-    metrics['MP4'] = extension_metrics['mp4']
-    metrics['MP4_PERCENT'] = extension_metrics['mp4'] / total
-    metrics['MKV'] = extension_metrics['mkv']
-    metrics['MKV_PERCENT'] = extension_metrics['mkv'] / total
-    # resolution_metrics = df.groupby(['resolution']).size()
+    metrics['metric_groups'] = ['codec', 'extension', 'resolution']
+    for metric_group in metrics['metric_groups']:
+        new_metrics = calculate_group_metrics(df, metric_group)
+        metrics.update(new_metrics)
     return metrics
 
 def apply_library_template(dic_library):
@@ -74,26 +83,44 @@ def build_library_email(list_info, lib_name):
     mail = apply_library_template(lib_metrics)
     return mail
 
+def get_file_list(path, recursive=False):
+    result = []
+    for f in listdir(path):
+        full_path_f = join(path, f)
+        if isfile(full_path_f) and f.endswith(media_endings):
+            result.append(full_path_f)
+        elif isdir(full_path_f) and recursive:
+            result.extend(get_file_list(full_path_f, recursive))
+    return result
+
+
 start = time.time()
 
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
+    media_endings = tuple(config['video_extensions'])
 
 all_media = []
 libraries_mail = ""
 for media_library in config['libs']:
     library_name = media_library["name"]
+    print(f'processing {library_name}')
     media_path = media_library['path']
-    media_endings = tuple(config['video_extensions'])
-    media_files = [join(media_path, f) for f in listdir(media_path) \
-        if f.endswith(media_endings) and isfile(join(media_path, f))]
+    recursive = False
+    if 'recursive' in media_library:
+        recursive = media_library['recursive']
+    media_files = get_file_list(media_path, recursive)
 
     all_library_media = [get_media_info(video, library_name) for video in media_files]
     mail = build_library_email(all_library_media, library_name)
     libraries_mail += mail
     all_media.extend(all_library_media)
 
-#now do the same for the entire media collection
+# saves csv for later analysis
+df_all_media = pd.DataFrame(all_media)
+df_all_media.to_csv(f'report_{str(date.today())}.csv',sep=';', header=True)
+
+# do the same template for the entire media collection and add before all others
 libraries_mail = build_library_email(all_media, 'All') + libraries_mail
 
 report_elements = {}
